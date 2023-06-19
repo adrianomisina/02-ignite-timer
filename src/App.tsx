b6b9c16ddc7d8827cf582bcda4254524b9f3d4cc
Adriano Misina <adriano.misina@gmail.com>
2023-06-19 11:20:05 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Button from "./components/Button"
+
+
+const App = () => {
+  return (
+    <>
+      <Button variant='primary'/>
+      <Button variant='secondary'/>
+      <Button variant='success'/>
+      <Button variant='danger'/>
+      <Button />
+    </>
+  )
+}
+
+export default App
