d3184f95dbd24d371989568bd135b1b7da8fceda
Adriano Misina <adriano.misina@gmail.com>
2023-06-21 14:39:44 -0300

    layout de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import Button from './components/Button'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
-
 import { defaultTheme } from './styles/theme/default'
 import { GlobalStyle } from './styles/global'
+import Router from './Router'
 
 const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="success" />
-      <Button variant="danger" />
-      <Button />
-
-      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
