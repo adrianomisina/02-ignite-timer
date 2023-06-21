eac5b41f47fa476681a64dbf155d975c6544fe1f
Adriano Misina <adriano.misina@gmail.com>
2023-06-21 16:50:28 -0300

    phospho instalation

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+import { HeaderContainer } from './styles'
+
+const Header = () => {
+  return (
+    <HeaderContainer>
+      <span>Logo</span>
+      <nav>
+        <a href="">timer</a>
+        <a href="">history</a>
+      </nav>
+    </HeaderContainer>
+  )
+}
+
+export default Header
